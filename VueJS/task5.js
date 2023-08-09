b02da9082ea3fab08f89e785531684071b5b7556
h3770k1k1 <amagdarass@gmail.com>
2023-08-09 17:57:18 +0200

    task 5 - simplifying excerpt, done

--- VueJS/task5.js
+++ VueJS/task5.js
@@ -2,3 +2,8 @@
 arrA
 	.filter((x) => !arrB.includes(x))
 	.concat(arrB.filter((x) => !arrA.includes(x)));
+
+// Alternatywny zapis
+ const uniqueElementsInArrA = arrA.filter(x => !arrB.includes(x));// Elementy unikalne w arrA
+ const uniqueElementsInArrB = arrB.filter(x => !arrA.includes(x)); // Elementy unikalne w arrB
+const combinedUniqueArrays = uniqueElementsInArrA.concat(uniqueElementsInArrB);// Połączenie unikalnych elementów z obu tablic
